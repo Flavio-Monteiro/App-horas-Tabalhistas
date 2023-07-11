dda32f1971e8a8eb3fba8f5f3bb546ef0844df7e
Flávio Monteiro <flavio.ms@outlook.com.br>
2023-07-11 12:22:33 -0300

    efeito glass adcionado

--- src/assets/myCompontents/Header.jsx
+++ src/assets/myCompontents/Header.jsx
@@ -1,6 +1,7 @@
 // Componente do Cabeçalho
 const Header = () => (
     <div className="header">
+        <img src="/public/calculadora.png" alt="" />
       <h1>Calculadora Trabalhista</h1>
     </div>
   );
--- src/assets/myCompontents/Subheader .jsx
+++ src/assets/myCompontents/Subheader .jsx
@@ -1,7 +1,8 @@
 // Componente do Subcabeçalho
 const Subheader = () => (
     <div className="subheader">
-      <h2>Escolha um serviço</h2>
+    
+      <h2>Escolha um Serviço</h2>
     </div>
   );
   export {Subheader}
